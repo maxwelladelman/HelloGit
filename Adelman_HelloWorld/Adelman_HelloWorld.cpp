f9f6f1ab41dffa9053431735d8cc16d48711030f
max_desktop <maxwell.adelman@gmail.com>
2023-03-11 18:56:06 -0500

    Add project files.

---
+++ Adelman_HelloWorld/Adelman_HelloWorld.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+//Maxwell Adelman
+//3/11/2022
+//CIS 1202.501
+//source code management  assignment
+int main() {
+
+
+	cout << "Hello, World!";
+
+
+
+
+
+
+
+
+
+
+ }
